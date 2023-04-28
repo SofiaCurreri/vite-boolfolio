bbb58dbf8b6449f464b3f71b937e7b7097741b92
SofiaCurreri <sofiacurreri@gmail.com>
2023-04-28 16:37:56 +0200

    added navbar

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<template>
+  <nav class="navbar bg-body-tertiary">
+    <div class="container">
+      <span class="navbar-brand mb-0 h1">Vue Portfolio</span>
+    </div>
+  </nav>
+</template>
+
+<style lang="scss" scoped></style>
