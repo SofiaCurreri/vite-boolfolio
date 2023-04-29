116be2b568186ef4ca63c8504b2514878e8e6e07
SofiaCurreri <sofiacurreri@gmail.com>
2023-04-29 13:09:03 +0200

    list initial structure + props

---
+++ src/components/ProjectList.vue
@@ -0,0 +1,25 @@
+<script>
+import ProjectCard from "./ProjectCard.vue";
+
+export default {
+  props: {
+    projects: Array,
+    title: String,
+  },
+
+  components: { ProjectCard, ProjectCard },
+};
+</script>
+
+<template>
+  <div class="container">
+    <h1>{{ title }}</h1>
+    <ProjectCard
+      v-for="project in projects"
+      :key="project.id"
+      :project="project"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
