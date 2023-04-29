b7ad819a2c50ba8dcc1a293d9e0e8e902d33746e
SofiaCurreri <sofiacurreri@gmail.com>
2023-04-29 13:08:31 +0200

    added components

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,37 @@
 <script>
+import axios from "axios";
 import AppHeader from "./components/AppHeader.vue";
+import ProjectList from "./components/ProjectList.vue";
 
 export default {
   data() {
     return {
-      title: "Hello world",
+      title: "Vue Portfolio",
+      projects: [],
     };
   },
 
-  components: { AppHeader },
+  components: { AppHeader, ProjectList },
+
+  methods: {
+    fetchProjects() {
+      axios.get("http://127.0.0.1:8000/api/projects").then((response) => {
+        this.projects = response.data;
+      });
+    },
+  },
+
+  created() {
+    this.fetchProjects();
+  },
 };
 </script>
 
 <template>
-  <AppHeader />
-  <h1>{{ title }}</h1>
+  <AppHeader :title="title" />
+
+  <!-- Il primo projects è quello che arriva dal figlio(ProjectList) mentre il secondo è quello del padre(App) scritto nei data -->
+  <ProjectList :projects="projects" title="Projects List" class="my-5" />
 </template>
 
 <style lang="scss" scoped></style>
