22244615edfe97c11606819f3b4797689c4baab0
SofiaCurreri <sofiacurreri@gmail.com>
2023-04-30 18:32:32 +0200

    import pages + created routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,25 @@
 import { createRouter, createWebHistory } from "vue-router";
+
+//import pages
+import HomePage from "../pages/HomePage.vue";
+import ContactPage from "../pages/ContactPage.vue";
+
+//creazione routes
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomePage,
+    },
+    {
+      path: "/contacts",
+      name: "contacts",
+      component: ContactPage,
+    },
+  ],
+});
+
+//export router
+export { router };
