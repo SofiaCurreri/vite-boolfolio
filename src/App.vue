3789d14d1d950f55a15267574eac91e892f5920e
SofiaCurreri <sofiacurreri@gmail.com>
2023-04-30 18:32:54 +0200

    divisione per pertinenza

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,22 @@
 <script>
-import axios from "axios";
 import AppHeader from "./components/AppHeader.vue";
-import ProjectList from "./components/ProjectList.vue";
+import HomePage from "./pages/HomePage.vue";
+import ContactPage from "./pages/ContactPage.vue";
 
 export default {
   data() {
     return {
       title: "Vue Portfolio",
-      projects: {
-        list: [],
-        pages: [],
-      },
     };
   },
 
-  components: { AppHeader, ProjectList },
-
-  emits: ["changePage"],
-
-  methods: {
-    fetchProjects(endpoint = null) {
-      //se non ti arriva endpoint usa questo
-      if (!endpoint) endpoint = "http://127.0.0.1:8000/api/projects";
-
-      //altrimenti...
-      axios.get(endpoint).then((response) => {
-        this.projects.list = response.data.data;
-        this.projects.pages = response.data.links;
-      });
-    },
-  },
-
-  created() {
-    this.fetchProjects();
-  },
+  components: { AppHeader, HomePage, ContactPage },
 };
 </script>
 
 <template>
   <AppHeader :title="title" />
-
-  <!-- Il primo projects è quello che arriva dal figlio(ProjectList) mentre il secondo è quello del padre(App) scritto nei data -->
-  <ProjectList
-    :projects="projects.list"
-    :pages="projects.pages"
-    @changePage="fetchProjects"
-    title="Most recent"
-    class="my-5"
-  />
+  <router-view></router-view>
 </template>
 
 <style lang="scss" scoped></style>
