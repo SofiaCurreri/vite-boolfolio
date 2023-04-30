b2c0f3b5e24cb6cc5dd20b7f6bde6cbfe90d9e0c
SofiaCurreri <sofiacurreri@gmail.com>
2023-04-30 11:18:08 +0200

    pagination

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,26 @@ export default {
   data() {
     return {
       title: "Vue Portfolio",
-      projects: [],
+      projects: {
+        list: [],
+        pages: [],
+      },
     };
   },
 
   components: { AppHeader, ProjectList },
 
+  emits: ["changePage"],
+
   methods: {
-    fetchProjects() {
-      axios.get("http://127.0.0.1:8000/api/projects").then((response) => {
-        this.projects = response.data;
+    fetchProjects(endpoint = null) {
+      //se non ti arriva endpoint usa questo
+      if (!endpoint) endpoint = "http://127.0.0.1:8000/api/projects";
+
+      //altrimenti...
+      axios.get(endpoint).then((response) => {
+        this.projects.list = response.data.data;
+        this.projects.pages = response.data.links;
       });
     },
   },
@@ -31,7 +41,13 @@ export default {
   <AppHeader :title="title" />
 
   <!-- Il primo projects è quello che arriva dal figlio(ProjectList) mentre il secondo è quello del padre(App) scritto nei data -->
-  <ProjectList :projects="projects" title="Most recent" class="my-5" />
+  <ProjectList
+    :projects="projects.list"
+    :pages="projects.pages"
+    @changePage="fetchProjects"
+    title="Most recent"
+    class="my-5"
+  />
 </template>
 
 <style lang="scss" scoped></style>
--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -4,6 +4,7 @@ import ProjectCard from "./ProjectCard.vue";
 export default {
   props: {
     projects: Array,
+    pages: Array,
     title: String,
   },
 
@@ -24,6 +25,23 @@ export default {
       />
     </div>
     <h2 v-else class="text-muted my-3">Non ci sono progetti pubblicati</h2>
+
+    <nav aria-label="Projects pagination">
+      <ul class="pagination my-4">
+        <li v-for="page in pages" class="page-item">
+          <button
+            type="button"
+            class="page-link"
+            @click="$emit('changePage', page.url)"
+            :class="{
+              disabled: !page.url,
+              active: page.active,
+            }"
+            v-html="page.label"
+          ></button>
+        </li>
+      </ul>
+    </nav>
   </section>
 </template>
 
