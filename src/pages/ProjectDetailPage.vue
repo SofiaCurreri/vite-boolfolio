dc73c3cbf2baecd34d8b3f3411e9cccf10106c49
SofiaCurreri <sofiacurreri@gmail.com>
2023-05-01 18:47:21 +0200

    managind card text and button

--- src/pages/ProjectDetailPage.vue
+++ src/pages/ProjectDetailPage.vue
@@ -1,13 +1,36 @@
 <script>
+import axios from "axios";
 import ProjectCard from "../components/ProjectCard.vue";
 
 export default {
+  name: "ProjectDetailPage",
+
+  data() {
+    return {
+      project: null,
+    };
+  },
+
   components: { ProjectCard },
+
+  created() {
+    //$route è una variabile di cui disponiamo solo se abbiamo il Router
+    //params perchè è un parametro
+    //id perchè è il parametro che ci serve
+    // console.log(this.$route.params.id);
+
+    axios
+      .get(`http://127.0.0.1:8000/api/projects/${this.$route.params.slug}`)
+      .then((response) => {
+        this.project = response.data;
+      });
+  },
 };
 </script>
 
 <template>
-  <h1>Dettaglio</h1>
+  <h1 class="my-4">Dettaglio Progetto {{ project?.title }}</h1>
+  <ProjectCard v-if="project" :project="project" :isDetail="true" />
 </template>
 
 <style lang="scss" scoped></style>
