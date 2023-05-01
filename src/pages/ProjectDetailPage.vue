29ec0785bbc3d1e0b33cf5109ee3378b96f44cb1
SofiaCurreri <sofiacurreri@gmail.com>
2023-05-01 13:51:54 +0200

    created ProjectDetailPage

---
+++ src/pages/ProjectDetailPage.vue
@@ -0,0 +1,13 @@
+<script>
+import ProjectCard from "../components/ProjectCard.vue";
+
+export default {
+  components: { ProjectCard },
+};
+</script>
+
+<template>
+  <h1>Dettaglio</h1>
+</template>
+
+<style lang="scss" scoped></style>
