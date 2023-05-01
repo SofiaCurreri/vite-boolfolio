508d9881edc4614f84358a04e1f55632d134a673
SofiaCurreri <sofiacurreri@gmail.com>
2023-05-01 13:52:02 +0200

    added router-link

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -15,7 +15,21 @@ export default {
       <div class="card-body">
         {{ project.text }}
       </div>
-      <div class="card-footer">Creato il {{ project.created_at }}</div>
+      <div class="card-footer d-flex justify-content-between">
+        Creato il {{ project.created_at }}
+
+        <router-link
+          class="btn btn-sm btn-primary"
+          :to="{
+            name: 'project-detail',
+            params: {
+              id: project.id,
+            },
+          }"
+        >
+          Vedi
+        </router-link>
+      </div>
     </div>
   </div>
 </template>
